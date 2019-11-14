e21b90f2b63a7f753fae3752b6b6e2d1ecacbf1b
jkeane889 <jkeane889@gmail.com>
2019-11-14 17:46:17 -0600

    wip - post feature

---
+++ client/src/components/Post.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import { SvgIcon } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: 'auto',
+    maxWidth: 500,
+  },
+  image: {
+    width: 128,
+    height: 128,
+  },
+  img: {
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  },
+}));
+
+export default function PostContainer() {
+  const styles = useStyles();
+
+  return (
+    <div className={styles.root}>
+      <Paper className={styles.paper}>
+        <Grid container spacing={2}>
+          <Grid item>
+            
+          </Grid>
+          
+          
+          
+        </Grid>
+      </Paper>
+    </div>
+  )
+}
+
+export default PostContainer;
--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,9 +1,49 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { SvgIcon } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
 
-export class Post extends React.Component {
-  render() {
-    return <h1>ToDo Post!</h1>;
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: "auto",
+    maxWidth: 500
+  },
+  image: {
+    width: 128,
+    height: 128
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%"
   }
+}));
+
+export default function PostContainer() {
+  const styles = useStyles();
+
+  return (
+    <div className={styles.root}>
+      <Paper className={styles.paper}>
+        <Grid container spacing={2}>
+          <Grid container item xs={12} spacing={3}>
+            <FormRow />
+          </Grid>
+          <Grid container item xs={12} spacing={3}>
+            <FormRow />
+          </Grid>
+          <Grid container item xs={12} spacing={3}>
+            <FormRow />
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
 }
 
-export default Post;
+export default PostContainer;
