f7bbb316ce81064f8ddf1a33d4e002f44105803a
Tye <tyemacon@gmail.com>
2019-11-14 12:18:40 -0600

    file structure for configuration key

---
+++ client/src/config/config-example.env.js
@@ -0,0 +1,11 @@
+/**
+ * CHANGE THE NAME of this file to config.env.js (or make a separate)
+ * Add the API KEY for google maps. 
+ * IF YOU DO NOT CHANGE THE NAME OF THE FILE AND COMMIT, THE API KEY
+ * WILL BE PUSHED TO GITHUB AND THE GITHUB, GOOGLE, AND GOVERNMENT GOONS
+ * WILL COME AFTER ME. SO CHANGE THE NAME
+ */
+
+module.exports = {
+  googleMapAPIKey: 'PASTE_KEY_HERE'
+}
