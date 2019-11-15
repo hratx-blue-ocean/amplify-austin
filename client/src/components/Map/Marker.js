4d1864b7618132c34b188044555cfc61680441d8
Tye Macon <tyemacon@gmail.com>
2019-11-14 18:30:22 -0600

    added map

--- client/src/components/Map/Marker.js
+++ client/src/components/Map/Marker.js
@@ -3,8 +3,8 @@ import React from "react";
 const defaultImageForFun =
   "https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg";
 
-// External CSS file can be implemented for more versatility, 
-// but until more definite icons are decided upon styling is 
+// External CSS file can be implemented for more versatility,
+// but until more definite icons are decided upon styling is
 // simply done within the function
 const Marker = ({ image }) => {
   const style = {
