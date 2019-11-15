07c953ed17a024d9e904c2a0be507971dbbc0573
Cameron Sobhani <XvxCxvX@gmail.com>
2019-11-15 15:20:03 -0600

    pairing with TYE (#18)

--- client/src/components/SignIn/SignIn.jsx
+++ client/src/components/SignIn/SignIn.jsx
@@ -1,7 +1,43 @@
 import React from "react";
 
 const SignIn = props => {
-  return <div>Howdy from SignIn</div>;
+  const checkLogIn = function (username, password) {
+    if (password.length >= 6 && password.length <= 16 && username.length >= 6 && username.length <= 16) {
+      return true;
+    }
+  };
+  //to do 
+  //verify account
+  //styling
+
+  return (
+    <div>
+      Create an Account
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        if (checkLogIn(e.target[0].value, e.target[1].value)) {
+          console.log('I`m in! =>', e.target[0].value, e.target[1].value)
+        }
+        else {
+          console.log('This password username combo is ass');
+        }
+      }}>
+        <div>
+          <input type={"text"} id={'username'} placeholder={'username'}></input>
+        </div>
+        <div>
+
+          <input type={"password"} placeholder={'password'}></input>
+        </div>
+        <div>
+
+          <input type={"submit"} value={"Sign Up"}></input>
+        </div>
+      </form>
+      <div>Don't have an account? <a href={''}>Sign Up</a></div>
+    </div>
+  );
 };
+
 
 export default SignIn;
