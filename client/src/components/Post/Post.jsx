703daf3847b509e62d266e0ffb98bb841b955f28
jkeane889 <jkeane889@gmail.com>
2019-11-15 10:29:09 -0600

    Updated Post component with appropriate materialUI library imports

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
 import { SvgIcon } from "@material-ui/core";
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles(theme => ({
@@ -45,4 +47,4 @@ export default function PostContainer(props) {
       </Paper>
     </div>
   );
-};
+}
