897270fff093fb42dcb38764824893d210809418
Tye Macon <tyemacon@gmail.com>
2019-11-15 17:09:04 -0600

    checkboxes added

--- client/src/components/SortFilter/Filter/Category/Category.js
+++ client/src/components/SortFilter/Filter/Category/Category.js
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "./Category.module.css";
 
 const Category = ({ category }) => {
-  return <div className={styles.container}>{category}</div>;
+  return (
+    <div className={styles.container}>
+      <input type="checkbox" value={category} />{category}
+    </div>
+  );
 };
 
 export default Category;
