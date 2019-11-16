dc48b4ec1356be94b7fa7d36260fd7b510874e9e
Seth <45956512+projectLewis@users.noreply.github.com>
2019-11-16 16:10:49 -0600

    Feature/postpage categoryname (#43)
    * minimum postpage functionality
    
    * prettier
    
    * prettier
    
    * commented out conflicts for travis ci
    
    * prettier
    
    * prettier again
    
    * update for test:

---
+++ client/src/components/PostPage/PostPageButtons/PostPageButtons.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { PostPageButtons } from "./PostPageButtons";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<PostPageButtons resolved={true} />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,15 @@
 const singlePost = require("./singlePost");
 const authentication = require("./authentication");
 const mainFilters = require("./mainFilters");
-const map = require('./map');
-const postStatus = require('./postStatus');
-const userSpecific = require('./userSpecific');
+const map = require("./map");
+const postStatus = require("./postStatus");
+const userSpecific = require("./userSpecific");
 
-module.exports = { singlePost, authentication, mainFilters, map, postStatus, userSpecific };
+module.exports = {
+  singlePost,
+  authentication,
+  mainFilters,
+  map,
+  postStatus,
+  userSpecific
+};
