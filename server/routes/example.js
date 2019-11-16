a3a39e27f24aa0e787e808922829c08f3a72ad5c
TAYLOR GEORGE <ATGEORGE@UTEXAS.EDU>
2019-11-16 11:15:28 -0600

    create post route completely finished

--- server/routes/example.js
+++ server/routes/example.js
@@ -5,30 +5,27 @@ const db = require("../../db/db");
 
 //testing catchall /api route
 
+
 router.get("/api", (req, res) => {
   res.send("connected to /api route")
 })
 
 
 router.post("/api/issue", (req, res) => {
-  console.log(req.body);
   const postInfo = req.body;
   if (typeof postInfo.location === 'string') {
     var apiStr = postInfo.location.replace(/ /g, '+');
     apiStr = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + apiStr + "&key=" + process.env.REACT_APP_GOOGLEMAPSAPIKEY
     //make api get request for atlong
-    console.log(apiStr);
     let lat;
     let lng;
     axios.get(apiStr)
       .then((result) => {
         lat = result.data.results[0].geometry.location.lat;
         lng = result.data.results[0].geometry.location.lng;
-        console.log(lat);
         return db.checkOtherFlag(postInfo.categoryName)
       })
       .then((result) => {
-        console.log("Now we're here!")
         const requestInput = {
           categoryName: postInfo.categoryName,
           creatorId: postInfo.creatorId,
@@ -42,8 +39,38 @@ router.post("/api/issue", (req, res) => {
         return db.addPost(requestInput)
       })
       .then((result) => {
-        console.log("RESULT: " + result);
         res.send(result);
+      })
+      .catch((err) => {
+        res.send(err)
+      });
+  } else {
+    let lat = postInfo.lat;
+    let lng = postInfo.lng;
+    let apiStr = lat + "," + lng;
+    apiStr = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + apiStr + "&key=" + process.env.REACT_APP_GOOGLEMAPSAPIKEY
+    //make api get request for atlong
+    let address;
+    axios.get(apiStr)
+      .then((result) => {
+        address = result.data.results[0].formatted_address;
+        return db.checkOtherFlag(postInfo.categoryName)
+      })
+      .then((result) => {
+        const requestInput = {
+          categoryName: postInfo.categoryName,
+          creatorId: postInfo.creatorId,
+          headline: postInfo.headline,
+          description: postInfo.description,
+          lat: lat,
+          lng: lng,
+          address: address,
+          otherFlag: result
+        }
+        return db.addPost(requestInput)
+      })
+      .then((result) => {
+        res.send({ insertId: result.insertId });
       })
       .catch((err) => {
         console.log(err);
