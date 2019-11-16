06a632e4161e9c2b115a5a8413b02a6f571f0259
Jonathan Keane <jkeane889@gmail.com>
2019-11-15 20:17:43 -0600

    Updated Post component and post container, updated package.json file with new dependencies for post component (#22)

---
+++ client/src/components/Posts/PostContainer.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Post from './Post.jsx';
+
+const containerStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+      overflow: 'hidden',
+      padding: theme.spacing(0, 3),
+    }
+}));
+
+const PostContainer = props => {
+    const postContStyles = containerStyles();
+
+    return (
+        <div className={postContStyles.root}>
+            {props.postData.map(post => {
+                return (
+                    <Post
+                        key={post.postID}
+                        type={post.type}
+                        category={post.category}
+                        title={post.headline}
+                        description={post.description}
+                        datecreated={post.created_at}
+                        location={post.location}
+                        votes={post.upvotes}
+                        username={post.username}
+                        status={post.status}
+                        favorited={post.favorited}
+                    />
+                )
+            })}
+        </div>
+    );
+}
+
+export default PostContainer;
