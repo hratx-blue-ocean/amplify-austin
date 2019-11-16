ca902ff0fff45a0984d78aa85bb6ab7278cd1376
atgeorge11 <53580812+atgeorge11@users.noreply.github.com>
2019-11-16 11:45:22 -0600

    Create post (#34)
    * server api route up and running
    
    * create createPost route
    
    * create post route completely finished
    
    * cleanup
    
    * save changes to db

--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ app.use((_, res, next) => {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(logger('dev'));
 
