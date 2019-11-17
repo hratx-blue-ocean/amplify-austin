59137c81e10d929fb53d45e239ed7e0815e29b05
atgeorge11 <53580812+atgeorge11@users.noreply.github.com>
2019-11-16 21:28:23 -0600

    Main route (#51)
    * main route
    
    * refactor database helpers methods

---
+++ db/db_interactions/mainFilters.js
@@ -0,0 +1,59 @@
+const connection = require('../db');
+
+const getPosts = function (userId, selectBy, sortBy, categories) {
+    return new Promise((resolve, reject) => {
+        let queryString = "SELECT id, headline, categoryId, created_at, eventDate, upvotes, address, status, otherFlag FROM posts "
+        let categoryClause = "";
+        let orderByClause = "";
+        let selectByClause = "";
+        let whereClauseCounter = 0;
+
+        if (categories.length > 0) {
+            let categoryOptions = "";
+            for (let i = 0; i < categories.length; i++) {
+                categoryOptions += "\'" + categories[i] + "\'";
+                if (i < categories.length - 1) {
+                    categoryOptions += ", ";
+                }
+            }
+            categoryClause = "categoryId IN (SELECT id FROM categories WHERE name IN (" + categoryOptions + ")) ";
+            whereClauseCounter++;
+        }
+
+        if (selectBy === "myPosts") {
+            selectByClause = "creatorId = " + userId + " ";
+            whereClauseCounter++;
+        } else if (selectBy === "favorites") {
+            selectByClause = "id IN (SELECT postId FROM favorites WHERE userId = " + userId + ") ";
+            whereClauseCounter++;
+        }
+
+        if (sortBy === "date") {
+            orderByClause = "ORDER BY created_at DESC";
+        } else {
+            orderByClause = "ORDER BY upvotes DESC";
+        }
+
+        if (whereClauseCounter > 0) {
+            queryString += "WHERE ";
+        }
+
+        queryString += categoryClause;
+
+        if (whereClauseCounter === 2) {
+            queryString += "AND ";
+        }
+
+        queryString += selectByClause + orderByClause + ";";
+
+        connection.query(queryString, (err, value) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(value);
+            }
+        })
+    })
+}
+
+module.exports = { getPosts };
--- server/routes/mainFilters.js
+++ server/routes/mainFilters.js
@@ -3,4 +3,60 @@ const router = require("express").Router();
 const axios = require("axios");
 const db = require("../../db/db_interactions");
 
+router.get("/api/main", (req, res) => {
+    const userId = req.query.userId;
+    const selectBy = req.query.selectBy || null;
+    const sortBy = req.query.sortBy || "popularity";
+    let categories = "";
+    if (req.query.categories) {
+        categories = req.query.categories !== "" ? req.query.categories.split("/") : "";
+    }
+
+
+    db.mainFilters.getPosts(userId, selectBy, sortBy, categories)
+        .then((rows) => {
+            let count = 0;
+            let posts = [];
+
+            const formatPost = function () {
+                if (count < rows.length) {
+
+                    posts[count] = {
+                        postId: rows[count].id,
+                        headline: rows[count].headline,
+                        created_at: rows[count].created_at,
+                        upvotes: rows[count].upvotes,
+                        status: rows[count].status,
+                        resolved: rows[count].resolved,
+                        otherFlag: rows[count].otherFlag,
+                        eventDate: rows[count].eventDate,
+                    }
+
+                    db.helpers.getCanAmplify(rows[count].id, userId)
+                        .then((bool) => {
+                            posts[count].canAmplify = bool;
+                            return db.helpers.getFavorited(rows[count].id, userId)
+                        })
+                        .then((bool) => {
+                            posts[count].favorited = bool;
+                            return db.helpers.getCategoryName(rows[count].categoryId)
+                        })
+                        .then((categoryName) => {
+                            posts[count].categoryName = categoryName;
+                            count++;
+                            formatPost();
+                        })
+                } else {
+                    res.send(posts);
+                }
+            }
+            formatPost();
+        })
+        .catch(err => {
+            console.log(err);
+            res.send(err);
+        })
+})
+
+
 module.exports = router;
