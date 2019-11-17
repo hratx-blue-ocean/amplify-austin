da6f637e901c41f6cffacbb6fe61e6f8c4b2a9ea
EthanHogan <ethan.hogan7@gmail.com>
2019-11-17 14:29:22 -0600

    add lat lng keys to response object and updated query to do so. also refactored route a bit to read a little cleaner

--- server/routes/mainFilters.js
+++ server/routes/mainFilters.js
@@ -5,6 +5,7 @@ const db = require("../../db/db_interactions");
 
 router.get("/api/main", (req, res) => {
   const userId = req.query.userId;
+  console.log(req.query)
   const selectBy = req.query.selectBy || null;
   const sortBy = req.query.sortBy || "popularity";
   let categories = "";
@@ -19,28 +20,31 @@ router.get("/api/main", (req, res) => {
       let count = 0;
       let posts = [];
 
-      const formatPost = function() {
+      const formatPost = function () {
         if (count < rows.length) {
+          let currentRow = rows[count];
           posts[count] = {
-            postId: rows[count].id,
-            headline: rows[count].headline,
-            created_at: rows[count].created_at,
-            upvotes: rows[count].upvotes,
-            status: rows[count].status,
-            resolved: rows[count].resolved,
-            otherFlag: rows[count].otherFlag,
-            eventDate: rows[count].eventDate
+            postId: currentRow.id,
+            headline: currentRow.headline,
+            created_at: currentRow.created_at,
+            upvotes: currentRow.upvotes,
+            status: currentRow.status,
+            resolved: currentRow.resolved,
+            otherFlag: currentRow.otherFlag,
+            eventDate: currentRow.eventDate,
+            lat: currentRow.lat,
+            lng: currentRow.lng,
           };
 
           db.helpers
-            .getCanAmplify(rows[count].id, userId)
+            .getCanAmplify(currentRow.id, userId)
             .then(bool => {
               posts[count].canAmplify = bool;
-              return db.helpers.getFavorited(rows[count].id, userId);
+              return db.helpers.getFavorited(currentRow.id, userId);
             })
             .then(bool => {
               posts[count].favorited = bool;
-              return db.helpers.getCategoryName(rows[count].categoryId);
+              return db.helpers.getCategoryName(currentRow.categoryId);
             })
             .then(categoryName => {
               posts[count].categoryName = categoryName;
