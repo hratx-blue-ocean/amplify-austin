f6e5bab493ea7eab339331d9c38036461d265eb6
Ethan Hogan <49109280+EthanHogan@users.noreply.github.com>
2019-11-16 22:19:58 -0600

    Map route (#52)
    * api map route and queries implemented
    
    Co-authored-by: Taylor <atgeorge@utexas.edu>
    
    * api map route and queries implemented
    
    Co-authored-by: Taylor <atgeorge@utexas.edu>

--- server/routes/map.js
+++ server/routes/map.js
@@ -1,6 +1,44 @@
 require("dotenv").config({ path: "../.env" });
 const router = require("express").Router();
-const axios = require("axios");
 const db = require("../../db/db_interactions");
+
+router.get("/api/map", (req, res) => {
+  console.log("Hello");
+  let categories = "";
+  if (req.query.categories) {
+    categories =
+      req.query.categories !== "" ? req.query.categories.split("/") : "";
+  }
+
+  console.log(db.map);
+
+  db.map
+    .getPostCoordinates(categories)
+    .then(rows => {
+      let count = 0;
+      let posts = [];
+
+      const formatPost = function() {
+        if (count < rows.length) {
+          posts[count] = {
+            postId: rows[count].id,
+            lat: rows[count].lat,
+            lng: rows[count].lng
+          };
+          db.helpers
+            .getCategoryName(rows[count].categoryId)
+            .then(categoryName => {
+              posts[count].categoryName = categoryName;
+              count++;
+              formatPost();
+            });
+        } else {
+          res.send(posts);
+        }
+      };
+      formatPost();
+    })
+    .catch(err => res.send(err));
+});
 
 module.exports = router;
