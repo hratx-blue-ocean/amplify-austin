b33c0315fe3bf4e52d7caf0944ca50bfad7dae65
Jonathan Keane <jkeane889@gmail.com>
2019-11-17 17:09:38 -0600

    Updated home page with sort and category fileter menus (#59)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import SignIn from "./components/SignIn/SignIn";
 import MapPage from "./components/MapPage/MapPage";
 import PostPage from "./components/PostPage/PostPage";
 import Create from "./components/Create/Create";
+import SortFilter from "./components/SortFilter/SortFilter";
+import axios from "axios";
 import { allIssues, firstPost } from "./FAKEDATA";
 import {
   BrowserRouter as Router,
@@ -21,9 +23,21 @@ export class App extends React.Component {
     this.state = {
       selectedPost: firstPost,
       posts: allIssues,
-      filteredCategories: []
+      filteredCategories: [],
+      sortSelection: "popularity"
     };
+
     this.saveFilters = this.saveFilters.bind(this);
+    this.sortBy = this.sortBy.bind(this);
+  }
+
+  sortBy(condition) {
+    let strCondition = condition;
+    console.log("sort by function called, this is condition: ", strCondition);
+    this.setState({
+      sortSelection: strCondition
+    });
+    // make axios call based on new state
   }
 
   // Pass this function down to any Filter Component
@@ -44,7 +58,10 @@ export class App extends React.Component {
           <div className={styles.component}>
             <Switch>
               <Route exact path="/">
-                {/* TO BE REPLACED BY THE HOME PAGE */}
+                <SortFilter
+                  sortBy={this.sortBy}
+                  saveFilters={this.saveFilters}
+                ></SortFilter>
                 <PostContainer
                   postData={this.state.posts}
                   saveFilters={this.saveFilters}
--- client/src/components/SortFilter/SortFilter.jsx
+++ client/src/components/SortFilter/SortFilter.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import Sort from "./Sort/Sort";
-import Filter from "./Filter/Filter";
+import CategorySelect from "./CategorySelect/CategorySelect.jsx";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 
-export class SortFilter extends React.Component {
-  render() {
-    return (
-      <div>
-        <Sort />
-        <Filter />
-      </div>
-    );
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
   }
-}
+}));
+
+const SortFilter = props => {
+  const menuStyles = useStyles();
+
+  return (
+    <div className={menuStyles.root}>
+      <Grid container spacing={3}>
+        <Sort item xs={6} sortBy={props.sortBy} />
+        <CategorySelect item xs={6} />
+      </Grid>
+    </div>
+  );
+};
 
 export default SortFilter;
