9a9ab72f576e96992207aba2f547d7a5498180df
Seth Lewis <lewiscatenary@gmail.com>
2019-11-18 02:46:11 -0600

    fixing stuff for travisCI

--- client/src/components/PostPage/PostPage.test.js
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import PostPage from "./PostPage";
-
-// it("renders without crashing", () => {
-//   const div = document.createElement("div");
-//   ReactDOM.render(<PostPage />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
--- client/src/components/PostPage/PostPageButtons/PostPageButtons.test.js
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import { PostPageButtons } from "./PostPageButtons";
-
-// it("renders without crashing", () => {
-//   const div = document.createElement("div");
-//   ReactDOM.render(<PostPageButtons resolved={true} />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
