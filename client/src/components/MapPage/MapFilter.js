04d7c24e37170358541d292e5334dcb842366a57
Tye Macon <tyemacon@gmail.com>
2019-11-18 12:12:47 -0600

    stole filter dropdown from jkeane and stuck it in

---
+++ client/src/components/MapPage/MapFilter.js
@@ -0,0 +1,33 @@
+import React from "react";
+import CategorySelect from "../SortFilter/CategorySelect/CategorySelect";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    textAlign: "center"
+  }
+}));
+
+const MapFilter = props => {
+  const menuStyles = useStyles();
+
+  return (
+    <div className={menuStyles.root}>
+      <Grid
+        container
+        spacing={12}
+        justify="center"
+        direction="row"
+        alignItems="center"
+      >
+        <Grid item xs={6}>
+          <CategorySelect selectCategories={props.selectCategories} />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default MapFilter;
