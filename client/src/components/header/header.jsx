88b9425866ebabebbd78b779eaad8c93020dc279
Seth Lewis <lewiscatenary@gmail.com>
2019-11-17 19:09:53 -0600

    updated package.json for db

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -36,9 +36,11 @@ const Header = props => {
         <div>
           <MenuIcon onClick={toggleMenu} style={{ height: 30, width: 30 }} />
         </div>
-        <div onClick={() => {
-          history.push("/home");
-        }}>
+        <div
+          onClick={() => {
+            history.push("/home");
+          }}
+        >
           <p>Amplify Austin</p> <p>{name}</p>
         </div>
         <div>
