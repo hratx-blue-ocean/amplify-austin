7ec7452ba9cd84bcebab174dce9cd683c312fbaf
James Jamail <james@jamesjamail.com>
2019-11-17 18:46:44 -0600

    updated poststatus queries/routes, created auth queriesroutes

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -3,4 +3,24 @@ const router = require("express").Router();
 const axios = require("axios");
 const db = require("../../db/db_interactions");
 
+router.post("/api/signup", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    console.log("signup route", username, password);
+    db.authentication.addUser(username, password)
+        .then((value) => {
+            res.send(value);
+        })
+});
+
+router.post("/api/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    console.log("login route");
+    db.authentication.login(username, password)
+        .then((value) => {
+            res.send(value);
+        })
+});
+
 module.exports = router;
