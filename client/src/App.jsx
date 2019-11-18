e732d4d4056301320cce36fbde70bfa976eb331d
Tye Macon <tyemacon@gmail.com>
2019-11-18 10:03:25 -0600

    matched selected posts with map

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -116,6 +116,7 @@ export class App extends React.Component {
               <PrivateRoute path="/create" component={Create} />
               <Route path="/map">
                 <MapPage
+                  posts={this.state.posts}
                   saveFilters={this.saveFilters}
                   filteredCategories={this.state.filteredCategories}
                 />
