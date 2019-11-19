04f11c496e421ae5d4b8eb98228eabf94d45322f
EthanHogan <ethan.hogan7@gmail.com>
2019-11-19 09:18:01 -0600

    pretty

--- client/src/components/Create/CurrentLocationButton.js
+++ client/src/components/Create/CurrentLocationButton.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import axios from 'axios';
-import { geolocated } from 'react-geolocated'
+import React from "react";
+import axios from "axios";
+import { geolocated } from "react-geolocated";
 
 class CurrentLocationButton extends React.Component {
   constructor(props) {
@@ -10,43 +10,47 @@ class CurrentLocationButton extends React.Component {
   render() {
     return (
       <div>
-        <button onClick={() => {
-          if (!this.props.isGeolocationAvailable) {
-            console.log('GeoLocation is not available on this browser!')
-          } else if (!this.props.isGeolocationEnabled) {
-            console.log('GeoLocation is not enabled')
-          } else {
-            // Nab Latitude and Longitude, send off to Google API for decoding
-            let key = `&key=${process.env.REACT_APP_MAP_API_KEY}`;
-            let https = `https://maps.googleapis.com/maps/api/geocode/json?latlng=`;
-            let coordinates = `${this.props.coords.latitude},${this.props.coords.longitude}`;
+        <button
+          onClick={() => {
+            if (!this.props.isGeolocationAvailable) {
+              console.log("GeoLocation is not available on this browser!");
+            } else if (!this.props.isGeolocationEnabled) {
+              console.log("GeoLocation is not enabled");
+            } else {
+              // Nab Latitude and Longitude, send off to Google API for decoding
+              let key = `&key=${process.env.REACT_APP_MAP_API_KEY}`;
+              let https = `https://maps.googleapis.com/maps/api/geocode/json?latlng=`;
+              let coordinates = `${this.props.coords.latitude},${this.props.coords.longitude}`;
 
-            const request = `${https}${coordinates}${key}`;
-            axios.get(request)
-              .then((results) => {
+              const request = `${https}${coordinates}${key}`;
+              axios.get(request).then(results => {
                 console.log(results);
-                let shortAddress = '';
-                const addressComponents = results.data.results[0].address_components
+                let shortAddress = "";
+                const addressComponents =
+                  results.data.results[0].address_components;
                 for (let i = 0; i < addressComponents.length; i++) {
                   let addressPart = addressComponents[i].long_name;
-                  if (addressPart === 'Austin' || addressPart === 'Texas') {
+                  if (addressPart === "Austin" || addressPart === "Texas") {
                     break;
                   } else {
                     shortAddress = `${shortAddress} ${addressPart}`;
                   }
                 }
                 this.props.setLocation(shortAddress.trim());
-              })
-          }
-        }}>Where I'm At</button>
+              });
+            }
+          }}
+        >
+          Where I'm At
+        </button>
       </div>
-    )
+    );
   }
 }
 
 export default geolocated({
   positionOptions: {
-    enableHighAccuracy: false,
+    enableHighAccuracy: false
   },
-  userDecisionTimeout: 5000,
+  userDecisionTimeout: 5000
 })(CurrentLocationButton);
