ed93fc1b8b5b78b0ab4bee84c2eff9c3010dee5b
Tye Macon <tyemacon@gmail.com>
2019-11-19 08:21:07 -0600

    current location nabbed

--- client/src/components/Create/CurrentLocationButton.js
+++ client/src/components/Create/CurrentLocationButton.js
@@ -17,7 +17,27 @@ class CurrentLocationButton extends React.Component {
           } else if (!this.props.isGeolocationEnabled) {
             console.log('Geolacation is not enabled')
           } else {
-            // console.log('here', this.props.coords);
+            // Nab Latitude and Longitude, send off to Google API for decoding
+            let key = `&key=${process.env.REACT_APP_MAP_API_KEY}`;
+            let https = `https://maps.googleapis.com/maps/api/geocode/json?latlng=`;
+            let coordinates = `${this.props.coords.latitude},${this.props.coords.longitude}`;
+
+            const request = `${https}${coordinates}${key}`;
+            axios.get(request)
+              .then((results) => {
+                console.log(results);
+                let shortAddress = '';
+                const addressComponents = results.data.results[0].address_components
+                for (let i = 0; i < addressComponents.length; i++) {
+                  let addressPart = addressComponents[i].long_name;
+                  if (addressPart === 'Austin' || addressPart === 'Texas') {
+                    break;
+                  } else {
+                    shortAddress = `${shortAddress} ${addressPart}`;
+                  }
+                }
+                console.log(shortAddress.trim());
+              })
           }
         }}>Where I'm At</button>
       </div>
