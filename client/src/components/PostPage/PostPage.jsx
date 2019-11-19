39cd9dcb470cf8344c94acbfd6f2f5cca942ccd2
Seth Lewis <lewiscatenary@gmail.com>
2019-11-19 17:17:13 -0600

    removed unused import

--- client/src/components/PostPage/PostPage.jsx
+++ client/src/components/PostPage/PostPage.jsx
@@ -108,8 +108,8 @@ const PostPage = props => {
               {fave === true ? (
                 <FilledStarIcon></FilledStarIcon>
               ) : (
-                  <EmptyStarIcon></EmptyStarIcon>
-                )}
+                <EmptyStarIcon></EmptyStarIcon>
+              )}
             </div>
           </div>
         </div>
