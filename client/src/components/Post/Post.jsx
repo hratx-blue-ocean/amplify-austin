13d39d37669691819f9b159bd9e7ff49b8015076
Tye Macon <tyemacon@gmail.com>
2019-11-21 15:01:50 -0600

    added notification to watchlist on postpage

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -204,8 +204,8 @@ const Post = props => {
                     {fave === true ? (
                       <VisibilityIcon></VisibilityIcon>
                     ) : (
-                        <VisibilityOutlinedIcon></VisibilityOutlinedIcon>
-                      )}
+                      <VisibilityOutlinedIcon></VisibilityOutlinedIcon>
+                    )}
                   </div>
                 </Grid>
               </Grid>
